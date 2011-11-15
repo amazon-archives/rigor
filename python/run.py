ba5d7e4b5cfdd48262725f1fabf4e810be51cf8f
Kevin Rauwolf <kevin@blindsight.com>
2011-11-14 18:25:38 -0800

    Added support for json parameters on command line

--- python/run.py
+++ python/run.py
@@ -1,15 +1,20 @@
 import rigor.runner
 
 import argparse
+import json
 
 def main():
 	parser = argparse.ArgumentParser(description='Runs algorithm on relevant images')
 	parser.add_argument('domain', help='Domain to run')
-	parser.add_argument('parameters', help='Path to parameters file')
+	parser.add_argument('parameters', help='Path to parameters file, or JSON block containing parameters')
 	parser.add_argument('-l', '--limit', metavar='COUNT', required=False, help='Maximum number of images to use')
 	parser.add_argument('-r', '--random', action="store_true", default=False, required=False, help='Fetch images ordered randomly if limit is active')
 	args = parser.parse_args()
-	i = rigor.runner.Runner(args.domain, args.parameters, args.limit, args.random)
+	try:
+		parameters = json.loads(args.parameters)
+	except ValueError:
+		parameters = args.parameters
+	i = rigor.runner.Runner(args.domain, parameters, args.limit, args.random)
 	for result in i.run():
 		print("\t".join([str(x) for x in result]))
 
