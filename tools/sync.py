fe7cf332a2d9a6c9072f9b1a564d1bf98501f496
Kevin Rauwolf <kevin@blindsight.com>
2012-01-18 15:17:26 -0800

    Added sync.py for importing

---
+++ tools/sync.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+Synchronizes files from the import directory to the image directory.  Used to
+allow users write access to the import directory, while retaining a read-only
+image repository.
+"""
+
+from rigor.config import config
+
+import os
+import subprocess
+import sys
+
+class Synchronizer(object):
+	""" Class containing methods for importing images into the Rigor framework """
+
+	extensions = ('jpg', 'png')
+	""" List of file extensions to synchronize """
+
+	def __init__(self, move=True):
+		self._source = os.path.abspath(config.get('import', 'upload_repository')) + os.sep
+		self._destination = os.path.abspath(config.get('global', 'image_repository')) + os.sep
+		self._move = move
+
+		rsync = os.path.abspath(config.get('import', 'rsync_path'))
+
+		args = [rsync, '-q', '-r', '-p', '--chmod=ugo+r,Dugo+x,ug+w', '-t', '-O']
+		if move:
+			args.append('--remove-source-files')
+		args.append(self._source)
+		args.append(self._destination)
+		self._args = args
+		os.umask(002)
+
+	def run(self):
+		""" Copies or moves all image files from source to destination directory """
+		return subprocess.call(self._args)
+
+if __name__ == '__main__':
+	s = Synchronizer()
+	sys.exit(s.run())
