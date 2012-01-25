444f6e7abe31ed8483312d135f42ce2ec39732f8
Kaolin Fire <kaolin@blindsight.com>
2012-01-25 15:18:34 -0800

    tweaks to text-roc-curve.php

--- python/examples/text-roc-curve.php
+++ python/examples/text-roc-curve.php
@@ -1,24 +1,27 @@
 <?php
+	ini_set('memory_limit','1000M');
 	/* SEE text-roc.py; Kevin says "JUST EW!"; Kaolin shrugs, and tries to hide the code. */
-	$foo = file('../results/results.txt');
+	$foo = file('results.txt');
 	$accum = Array();
 	foreach ($foo as $row) {
-		list($parameter,$expected,$detected) = explode("\t",$row);
+		list($parameter,$image,$expected,$detected) = explode("\t",$row);
 		if (!isset($accum["".$paramater])) {
 			//$accum["".$parameter] = Array ("falsepositive"=>0,"truepositive"=>0,"count"=>0);
 		}
 		if ($expected && $detected) {
 			$accum["".$parameter]["truepositive"] ++;
-			$accum["".$parameter]["count"] ++;
 		} else if (!$expected && $detected) {
 			$accum["".$parameter]["falsepositive"] ++;
 		} else if ($expected && !$detected) {
-			$accum["".$parameter]["count"] ++;
+			$accum["".$parameter]["falsenegative"] ++;
+		} else if (!$expected && !$detected) {
+			$accum["".$parameter]["truenegative"] ++;
 		}
 	}
+	// TPR = TP / P = TP / (TP + FN)
+	// FPR = FP / N = FP / (FP + TN)
 	foreach ($accum as $key=>$value) {
-		if ($value["count"] == 0) $value["count"] = 1;
-		$accum[$key]["tprate"] = $accum[$key]["truepositive"]/$value["count"];
-		$accum[$key]["fprate"] = $accum[$key]["falsepositive"]/$value["count"];
+		$accum[$key]["tprate"] = $accum[$key]["truepositive"]/($accum[$key]["truepositive"] + $accum[$key]["falsenegative"]);
+		$accum[$key]["fprate"] = $accum[$key]["falsepositive"]/($accum[$key]["falsepositive"] + $accum[$key]["truenegative"]);
 	}
 	print_r($accum);
