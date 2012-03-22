75196629a7150c678d806f3e9c56946cbb1bbeaf
Dave Auerbach <dave@blindsight.com>
2012-03-22 15:14:26 -0700

    Added annotation tags, destroy image method. Fixed location format bug.

--- python/rigor/imageops.py
+++ python/rigor/imageops.py
@@ -3,6 +3,7 @@ Various utilities for dealing with images
 """
 
 from rigor.config import config
+from rigor.dbmapper import DatabaseMapper
 
 try:
 	import Image
@@ -23,7 +24,7 @@ def build_thumbnail_path(image, base_path, size, separator = os.sep):
 	"""
 	Given a base path, constructs a path to the image's thumbnail
 	"""
-	return os.extsep.join((separator.join((base_path, '{0}x{0}'.format(int(size)), image['locator'][0:2], image['locator'][2:4], image['locator'])), image['format']))
+	return build_path(image, os.path.join(base_path, '{0}x{0}'.format(int(size))), separator)
 
 def find(image):
 	"""
@@ -57,3 +58,23 @@ def read(path):
 	Returns a PIL image from reading a given path
 	"""
 	return Image.open(path)
+
+
+def destroy_image(database, image):
+	""" Completely Removes an image from the database, along with its tags, annotations, and any anotation tags. Also deletes the image file from disk."""
+	mapper=DatabaseMapper(database)
+	with database.get_cursor() as cursor:
+		sql = "DELETE FROM tag WHERE image_id = %s;"
+		cursor.execute(sql, (image['id'],))
+		annotations = mapper._get_annotations_by_image_id(cursor, image['id'])
+		for annotation in annotations:
+			sql="DELETE FROM annotation_tag WHERE annotation_id = %s"
+			cursor.execute(sql, (annotation['id'],))
+			mapper._delete_annotation(cursor, annotation['id'])
+		sql = "DELETE FROM image WHERE id = %s;"
+		cursor.execute(sql, (image['id'],))
+		image_path = find(image)
+		os.unlink(image_path)
+		for thumbnail_dir in os.listdir(config.get('thumbnail', 'image_repository')):
+			image_thumbnail_path = build_path(image, os.path.join(config.get('thumbnail', 'image_repository'), thumbnail_dir))
+			os.unlink(image_thumbnail_path)
