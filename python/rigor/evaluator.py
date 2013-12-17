6d9d104d587de2cfcfb118189c14092385415ac8
Kaolin Fire <kaolin@blindsight.com>
2013-12-17 14:06:50 -0800

    reimplemented match_detections

--- python/rigor/evaluator.py
+++ python/rigor/evaluator.py
@@ -15,7 +15,7 @@ class ObjectAreaEvaluator(object):
 	import numpy as np
 	from shapely.geometry import Polygon
 
-	def __init__(self, scatter_punishment=lambda(k): 0.8, precision_threshold=0.4, recall_threshold=0.8):
+	def __init__(self, scatter_punishment=lambda(k): 1.0, precision_threshold=0.4, recall_threshold=0.8):
 		"""
 		scatter_punishment is f_{sc}(k); "a parameter function of the evaluation
 		scheme which controls the amount of punishment which is inflicted in case
@@ -121,6 +121,78 @@ class ObjectAreaEvaluator(object):
 				recall_matrix[gt_index, det_index] = recall_area
 		return (precision_matrix, recall_matrix)
 
+	def match_detections(self, ground_truths, detections):
+		"""
+		gt_to_detections, detections_to_gt = evaluator.match_detections(ground_truths, detections)
+		debugDetail('gt to detection matches: %s' % str(gt_to_detections))
+		debugDetail('detection to gt matches: %s' % str(detections_to_gt))
+		return (
+			( False, (1, 2), (3, 5), (4,), (6,),  (6,) ),
+			( (2,), (2,), (3,), (4,), (3,), (5, 6))
+		)
+		"""
+		if not ground_truths or not detections:
+			return (0.,0.,(0.,len(detections)), (0., len(ground_truths)))
+
+		ground_truths, detections = ObjectAreaEvaluator.prune_and_polygon(ground_truths, detections)
+		ground_truth_count = len(ground_truths)
+		detection_count = len(detections)
+		if ground_truth_count == 0 or detection_count == 0:
+			return (0., 0., (0., float(detection_count)), (0., float(ground_truth_count)))
+
+		precision_matrix, recall_matrix = self.build_matrices(ground_truths, detections)
+		ground_truth_count = precision_matrix.shape[0]
+		detection_count = precision_matrix.shape[1]
+		ground_truth_sets_precision = defaultdict(set) # number of ground truth items that match a particular detection in the precision matrix
+		detection_sets_precision = defaultdict(set) # number of detection items that match a particular ground truth in the precision matrix
+		ground_truth_sets_recall = defaultdict(set) # number of ground truth items that match a particular detection in the recall matrix
+		detection_sets_recall = defaultdict(set) # number of detection items that match a particular ground truth in the recall matrix
+
+		for gt_index in range(ground_truth_count):
+			for det_index in range(detection_count):
+				if precision_matrix[gt_index, det_index] >= self.precision_threshold:
+					ground_truth_sets_precision[det_index].add(gt_index)
+					detection_sets_precision[gt_index].add(det_index)
+				if recall_matrix[gt_index, det_index] >= self.recall_threshold:
+					ground_truth_sets_recall[det_index].add(gt_index)
+					detection_sets_recall[gt_index].add(det_index)
+
+		match_ground_truth =  [[] for number in xrange(ground_truth_count)]
+		match_detection = [[] for number in xrange(detection_count)]
+
+		for gt_index in detection_sets_precision:
+			matching_detections_precision = detection_sets_precision[gt_index]
+			if len(matching_detections_precision) == 1:
+				(detection_precision, ) = matching_detections_precision
+				if len(ground_truth_sets_precision[detection_precision]) == 1:
+					match_ground_truth[gt_index].append(detection_precision)
+			else:
+				# one-to-many (one ground truth to many detections)
+				gt_sum = 0.
+				for detection_precision in matching_detections_precision:
+					gt_sum += recall_matrix[gt_index, detection_precision]
+				if gt_sum >= self.recall_threshold:
+					for detection_precision in matching_detections_precision:
+						match_ground_truth[gt_index].append(detection_precision)
+						match_detection[detection_precision].append(gt_index)
+		for det_index in ground_truth_sets_recall:
+			matching_ground_truths_recall = ground_truth_sets_recall[det_index]
+			if len(matching_ground_truths_recall) == 1:
+				(ground_truth_recall, ) = matching_ground_truths_recall
+				if len(detection_sets_recall[ground_truth_recall]) == 1:
+					match_detection[det_index].append(ground_truth_recall)
+			else:
+				# many-to-one (many ground truths covered by one detection)
+				det_sum = 0
+				for ground_truth_recall in matching_ground_truths_recall:
+					det_sum += precision_matrix[ground_truth_recall, det_index]
+				if det_sum >= self.precision_threshold:
+					for ground_truth_recall in matching_ground_truths_recall:
+						det_sum += precision_matrix[ground_truth_recall, det_index]
+						match_detection[det_index].append(ground_truth_recall)
+						match_ground_truth[ground_truth_recall].append(det_index)
+		return match_ground_truth, match_detection
+
 	def evaluate(self, ground_truths, detections):
 		"""
 		Given lists of polylines for each parameter (ground_truths, detections),
@@ -131,8 +203,11 @@ class ObjectAreaEvaluator(object):
 
 		>>> e = ObjectAreaEvaluator()
 		>>> e.evaluate(gt, det)
-		(0.2571428571428572, 0.36, (1.8, 7.0), (1.8, 5.0))
+		(0.5714285714285714, 0.8, (4.0, 7.0), (4.0, 5.0))
 		"""
+
+		if not ground_truths or not detections:
+			return (0.,0.,(0.,len(detections)), (0., len(ground_truths)))
 
 		ground_truths, detections = ObjectAreaEvaluator.prune_and_polygon(ground_truths, detections)
 		ground_truth_count = len(ground_truths)
@@ -153,8 +228,8 @@ class ObjectAreaEvaluator(object):
 
 		>>> e = ObjectAreaEvaluator()
 		>>> precision = e.np.array([[ 1., 0., 0., 0., 0.,], \
-			[0., .4, 0., 0., 0.,], \
-			[0., .4, 1., 0., 0.,], \
+			[0., .3, 0., 0., 0.,], \
+			[0., .3, 1., 0., 0.,], \
 			[0., 0., 0., 1., 1.,], \
 			])
 		>>> recall = e.np.array([[1., 0., 0., 0., 0.,], \
@@ -192,12 +267,14 @@ class ObjectAreaEvaluator(object):
 				if len(ground_truth_sets_precision[detection_precision]) == 1:
 					one_to_one_precision.add((gt_index, detection_precision))
 			else:
-				# one-to-many
+				# one-to-many (one ground truth to many detections)
 				gt_sum = 0.
 				for detection_precision in matching_detections_precision:
 					gt_sum += recall_matrix[gt_index, detection_precision]
 				if gt_sum >= self.recall_threshold:
+					#print("1:N ~ GT {} : DT {}".format(gt_index,matching_detections_precision))
 					match_ground_truth += self.scatter_punishment(matching_detections_precision)
+					match_detection += len(matching_detections_precision) * self.scatter_punishment(matching_detections_precision)
 
 		one_to_one_recall = set()
 		for det_index in ground_truth_sets_recall:
@@ -207,12 +284,14 @@ class ObjectAreaEvaluator(object):
 				if len(detection_sets_recall[ground_truth_recall]) == 1:
 					one_to_one_recall.add((ground_truth_recall, det_index))
 			else:
-				# many-to-one
+				# many-to-one (many ground truths covered by one detection)
 				det_sum = 0
 				for ground_truth_recall in matching_ground_truths_recall:
 					det_sum += precision_matrix[ground_truth_recall, det_index]
 				if det_sum >= self.precision_threshold:
+					#print("N:1 ~ DT {} : GT {}".format(det_index,matching_ground_truths_recall))
 					match_detection += self.scatter_punishment(matching_ground_truths_recall)
+					match_ground_truth += len(matching_ground_truths_recall) * self.scatter_punishment(matching_ground_truths_recall)
 
 		one_to_one_matches = one_to_one_precision & one_to_one_recall
 		match_ground_truth += len(one_to_one_matches)
@@ -240,5 +319,7 @@ if __name__ == '__main__':
 			((9, 20), (12, 20), (12, 22), (9, 22)),
 			((17, 17), (20, 17), (20, 20), (17, 20)),
 			((17, 10), (19, 10), (19, 15), (17, 15)),
-		)
+		),
+		'gt2':  [[(146, 169), (614, 169), (614, 371), (146, 371)], [(22, 226), (108, 226), (108, 260), (22, 260)], [(123, 235), (151, 235), (151, 251), (123, 251)], [(166, 235), (202, 235), (202, 261), (166, 261)], [(217, 228), (305, 228), (305, 258), (217, 258)]],
+		'det2': [((12, 264), (12, 221), (137, 221), (137, 264)), ((139, 364), (139, 175), (534, 175), (534, 364))],
 	})
