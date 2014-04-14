f133ac7e6efc34b0d09b77d0cc72613c411212e2
Kevin Rauwolf <kevin@blindsight.com>
2014-04-14 16:22:12 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import glob
+import os
+
+scripts = glob.glob(os.path.join('tools', '*.py'))
+scripts.append('python/import.py')
+sql_files = glob.glob(os.path.join('sql', '*.sql'))
+
+setup(name='Rigor',
+	version='1.0.1',
+	description='The Rigor testing framework',
+	author='Kevin Rauwolf',
+	author_email='kevin@blindsight.com',
+	url='https://github.com/blindsightcorp/rigor',
+	package_dir = {'': 'python'},
+	packages=['rigor',],
+	scripts=scripts,
+	data_files=[('config', 'rigor.ini.sample'),
+		('sql', sql_files),
+	],
+)
