d47f19f9ef4d3a035bf0d808474c47cb30d33053
Kevin Rauwolf <kevin@blindsight.com>
2014-08-08 10:38:59 -0700

    Added checkpoint feature

--- python/rigor/runner.py
+++ python/rigor/runner.py
@@ -7,9 +7,11 @@ from rigor.config import config
 import rigor.logger
 import rigor.database
 import rigor.dbmapper
+from rigor.checkpoint import Checkpoint, Checkpointer
 
 import abc
 import json
+import sys
 import argparse
 
 class BaseRunner(object):
@@ -24,6 +26,7 @@ class BaseRunner(object):
 		self._algorithm = algorithm
 		self._parameters = None
 		self._arguments = None
+		self._checkpoint = None
 		self.parse_arguments(arguments)
 		self._algorithm.set_arguments(self._arguments)
 		self.load_parameters()
@@ -35,13 +38,18 @@ class BaseRunner(object):
 
 		Sets self._arguments with the parsed argument object
 		"""
-		parser = argparse.ArgumentParser(description='Runs algorithm on relevant images', conflict_handler='resolve')
-		parser.add_argument('-p', '--parameters', required=False, help='Path to parameters file, or JSON block containing parameters')
-		self.add_arguments(parser)
-		if arguments:
-			self._arguments = parser.parse_args(arguments)
+		checkpoint_parser = argparse.ArgumentParser(add_help=False)
+		checkpoint_parser.add_argument('-c', '--checkpoint', type=argparse.FileType('rb'), required=False, help='Resume from checkpoint file. If you specify this, all other options will be ignored.')
+		checkpoint_args, remaining = checkpoint_parser.parse_known_args(arguments)
+		if checkpoint_args.checkpoint:
+			timestamp, arguments = Checkpointer.read_header(checkpoint_args.checkpoint)
+			self._arguments = arguments
+			self._checkpoint = checkpoint_args.checkpoint
 		else:
-			self._arguments = parser.parse_args()
+			parser = argparse.ArgumentParser(description='Runs algorithm on relevant images', conflict_handler='resolve', usage='%(prog)s {-c | [options]}', parents=[checkpoint_parser, ])
+			parser.add_argument('-p', '--parameters', required=False, help='Path to parameters file, or JSON block containing parameters')
+			self.add_arguments(parser)
+			self._arguments = parser.parse_args(remaining)
 
 	def add_arguments(self, parser):
 		"""
@@ -77,11 +85,33 @@ class BaseRunner(object):
 		pass
 
 	@abc.abstractmethod
-	def run(self):
+	def get_images(self):
 		"""
-		This is the method called to run the algorithm.  It must be overridden.
+		Fetches images to be run against the algorithm, with annotations included
+		to check against the results. It is called by the run() method, and must be
+		overridden.
 		"""
 		pass
+
+	def run(self):
+		""" This is the method called to run the algorithm. """
+		images = self.get_images()
+		if self._checkpoint:
+			checkpointer, checkpoint = Checkpointer.resume(self._checkpoint.name)
+			results = checkpoint.results
+			seen = checkpoint.seen
+			images = [image for image in images if image['id'] not in seen]
+		else:
+			checkpointer = Checkpointer(self._arguments)
+			results = list()
+			seen = set()
+		self._logger.debug('Processing {0} images (skipping {1} checkpointed)'.format(len(images), len(seen)))
+		with checkpointer:
+			for image in images:
+				result = self._algorithm.apply(image)
+				checkpointer.log(image['id'], result)
+				results.append(result)
+		return self.evaluate(results)
 
 	def evaluate(self, results): # pylint: disable=R0201
 		"""
@@ -98,21 +128,15 @@ class DatabaseRunner(BaseRunner):
 	"""
 	Runner base class that uses the database to discover images for evaluation
 	"""
-	__metaclass__ = abc.ABCMeta
 
 	def __init__(self, algorithm, arguments=None):
 		BaseRunner.__init__(self, algorithm, arguments)
 		self._database = rigor.database.Database.instance(self._arguments.database)
 		self._database_mapper = rigor.dbmapper.DatabaseMapper(self._database)
 
-	def parse_arguments(self, arguments):
-		"""
-		Parses command-line arguments
-
-		Sets self._arguments with the parsed argument object
-		"""
-		parser = argparse.ArgumentParser(description='Runs algorithm on relevant images', conflict_handler='resolve')
-		parser.add_argument('-p', '--parameters', required=False, help='Path to parameters file, or JSON block containing parameters')
+	def add_arguments(self, parser):
+		""" Adds database-specific arguments """
+		super(DatabaseRunner, self).add_arguments(parser)
 		limit = parser.add_mutually_exclusive_group()
 		limit.add_argument('-l', '--limit', type=int, metavar='COUNT', required=False, help='Maximum number of images to use')
 		limit.add_argument('-i', '--image_id', type=int, metavar='IMAGE ID', required=False, help='Single image ID to run')
@@ -120,11 +144,6 @@ class DatabaseRunner(BaseRunner):
 		parser.add_argument('--tag_require', action='append', dest='tags_require', required=False, help='Tag that must be present on selected images')
 		parser.add_argument('--tag_exclude', action='append', dest='tags_exclude', required=False, help='Tag that must not be present on selected images')
 		parser.add_argument('database', help='Name of the database to use')
-		self.add_arguments(parser)
-		if arguments:
-			self._arguments = parser.parse_args(arguments)
-		else:
-			self._arguments = parser.parse_args()
 
 class Runner(DatabaseRunner):
 	""" Class for running algorithms against test images """
@@ -141,13 +160,14 @@ class Runner(DatabaseRunner):
 		self._domain = domain
 		self._image_id = None
 
-	def run(self):
-		""" Runs the algorithm on the images matching the supplied arguments """
+	def get_images(self):
+		"""
+		Fetches images to be run against the algorithm from the database
+		"""
 		self._logger.debug('Fetching image IDs from database')
 		if self._arguments.image_id:
 			self._image_id = self._arguments.image_id
 			images = (self._database_mapper.get_image_for_analysis(self._domain, self._image_id), )
 		else:
 			images = self._database_mapper.get_images_for_analysis(self._domain, self._arguments.limit, self._arguments.random, self._arguments.tags_require, self._arguments.tags_exclude)
-		self._logger.debug('Processing {0} images'.format(len(images)))
-		return self.evaluate(map(self._algorithm.apply, images))
+		return images
